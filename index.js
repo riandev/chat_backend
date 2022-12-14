bf352fa04f1accaf1d1f3a5df63875a13065dcbd
riandev <rian.acca@gmail.com>
2022-12-14 12:52:19 +0600

    new

--- index.js
+++ index.js
@@ -34,54 +34,14 @@ io.on("connection", (socket) => {
     socket.on("join chat", (room) => {
         socket.join(room);
         console.log("User Joined Room: " + room);
+        console.log("User Joined Room: " + room);
     });
     socket.on("typing", (room) => socket.in(room).emit("typing"));
     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
     socket.on("new message", (newMessageReceived) => {
-
         let sended = socket.broadcast.emit('message received', newMessageReceived);
         console.log(sended);
         console.log(newMessageReceived);
-        // let chat = newMessageRecieved.chat;
-
-
-        // if (!chat.users) return console.log("chat.users not defined");
-
-        // chat.users.forEach((user) => {
-        //     if (user === newMessageRecieved.sender._id) return console.log("return");
-
-        //     socket.in(user).emit("message recieved", newMessageRecieved);
-        // });
     });
-
-    // socket.off("setup", (userData) => {
-    //     console.log("USER DISCONNECTED");
-    //     socket.leave(userData._id);
-    // });
 });
-
-
-
-
-
-
-// io.on('connection', (socket) => {
-//     console.log('a user Connected', socket.id);
-//     socket.on('disconnect', (reason) => {
-//         console.log('Disconnected:', reason);
-//     })
-//     socket.emit('messages', messages)
-//     socket.on('message', (msg) => {
-//         let payload = {
-//             id: new Date().getTime(),
-//             time: new Date().toISOString(),
-//             text: msg,
-//             user: 'Guest'
-//         }
-//         console.log(payload)
-//         messages.push(payload);
-//         socket.broadcast.emit('message', payload);
-//         socket.emit('message', payload);
-//     })
-// });
